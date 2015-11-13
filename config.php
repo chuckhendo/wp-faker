7b1faef87d92463449c0a90e8ba075d4f658d811
alanpilloud <alanp@rc2.ch>
2015-11-13 16:25:01 +0100

    Added twig and templates

--- config.php
+++ config.php
@@ -3,12 +3,12 @@
  *  Edit your configuration here 
  *  you can already use $faker
  */
-$faker = Faker\Factory::create('fr_CH'); // You can change that value to suit your needs
+$faker = Faker\Factory::create('en_GB'); // You can change that value to suit your needs
 
-define('POST_TYPE','post'); // What post type would you like to seed ?
-define('POST_AUTHOR',1); // What's the author id to use ?
-define('POST_TITLE',$faker->name()); // What's the post title you're gonna use ?
-define('POST_CONTENT',$faker->realText()); // What's the post title you're gonna use ?
+define('POST_TYPE', 'post'); // What post type would you like to seed ?
+define('POST_AUTHOR', 1); // What's the author id to use ?
+define('POST_TITLE', $faker->name()); // What's the post title you're gonna use ?
+define('POST_CONTENT', $faker->realText()); // What's the post title you're gonna use ?
 
 /**
  *  Acf values can be listed here.
@@ -16,7 +16,7 @@ define('POST_CONTENT',$faker->realText()); // What's the post title you're gonna
  *  The key is the name of the field.
  *  To use flex fields, use the field_key (you know, that field_5641e6b5d1167 kind of key)
  */
-/*$values = array(
+/*$acf_values = array(
     'lastname' => $faker->lastname(),
     'firstname' => $faker->lastname(),
     'company' => $faker->company()
--- index.php
+++ index.php
@@ -3,85 +3,60 @@ require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/config.php';
 require __DIR__. '/../wp-load.php';
 
+Twig_Autoloader::register();
+$loader = new Twig_Loader_Filesystem('templates');
+$twig = new Twig_Environment($loader);
+
 $faking_content_url = basename($_SERVER['PHP_SELF']);
 
-if(filter_input(INPUT_GET, 'proceed') == 1) {
+if (filter_input(INPUT_GET, 'proceed') == 1) {
     /**
      *  Generating content !
      */
     $post_id = wp_insert_post(
     	array(
-    		'comment_status'	=>	'closed',
-    		'ping_status'		=>	'closed',
-    		'post_author'		=>	POST_AUTHOR,
-    		'post_name'		=>	sanitize_title(POST_TITLE),
-    		'post_title'		=>	POST_TITLE,
-            'post_content'		=>	POST_CONTENT,
-    		'post_status'		=>	'publish',
-    		'post_type'		=>	POST_TYPE
+    		'comment_status' => 'closed',
+    		'ping_status' => 'closed',
+    		'post_author' => POST_AUTHOR,
+    		'post_name' => sanitize_title(POST_TITLE),
+    		'post_title' =>	POST_TITLE,
+            'post_content' => POST_CONTENT,
+    		'post_status' => 'publish',
+    		'post_type' => POST_TYPE
     	)
     );
     
-    if(isset($values)) {
-        foreach($values as $k => $v) {
+    if(isset($acf_values)) {
+        foreach($acf_values as $k => $v) {
             update_field($k, $v, $post_id);
         }
     }
     
-    $content = '
-    <h3>Content created with success!</h3>
-    <p>The content <a href="'.get_permalink($post_id).'" target="_blank">"'.POST_TITLE.'"</a> with id '.$post_id.' has been created!</p>';
-    
-    $button = '<a href="'.$faking_content_url.'?proceed=1" class="btn btn-sm btn-primary">Great, one more time!</a> <small>or</small> <a href="'.$faking_content_url.'?proceed=0" class="btn btn-sm btn-secondary">what\'s my config again?</a>';
+    $data['post'] = [
+        'id' => $post_id,
+        'url' => get_permalink($post_id),
+        'title' => POST_TITLE,
+    ];
+
+    $data['footer'] = '<a href="'.$faking_content_url.'?proceed=1" class="btn btn-sm btn-primary">Great, one more time!</a> <small>or</small> <a href="'.$faking_content_url.'?proceed=0" class="btn btn-sm btn-secondary">what\'s my config again?</a>';
+    $template = 'go.twig';
     
 } else {
-
-
-
-
     /**
      *  Asking to confirm config
-     */    
-    $content = '
-    <h3>Ready to generate content with this config?</h3>
-    <ul>
-        <li>Post type : '.POST_TYPE.'</li>
-        <li>Post author id : '.POST_AUTHOR.'</li>
-        <li>Post content : '.POST_CONTENT.'</li>'.
-        ((isset($values))?'<li>Dummy Acf values example : <br/><pre>'.print_r($values,1).'</pre></li>':'').'
-    </ul>';
+     */
+    $data['config'] = [
+        'Post type : '.POST_TYPE,
+        'Post author id : '.POST_AUTHOR,
+        'Post content : '.POST_CONTENT,
+    ];
     
-    $button = '<a href="'.$faking_content_url.'?proceed=1" class="btn btn-sm btn-primary">OK, let\'s go!</a>';
+    if (isset($acf_values)) {
+        $data['config'][] = 'Dummy Acf values example : <br><pre>'.print_r($acf_values,1).'</pre>';
+    }
     
+    $data['footer'] = '<a href="'.$faking_content_url.'?proceed=1" class="btn btn-sm btn-primary">OK, let\'s go!</a>';
+    $template = 'ready.twig';
 }
 
-
-
-
-/**
- *  Display it all in a nice template =)
- */
-?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>Faking content</title>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha/css/bootstrap.min.css">
-    </head>
-    <body>
-        <div class="container m-y-lg">
-            <div class="col-sm-8 col-sm-offset-2">
-                <div class="card">
-                    <div class="card-block">
-                        <?= $content ?>
-                    </div>
-                    <div class="card-footer">
-                        <?= $button ?>
-                    </div>
-                </div>
-                <p><small><a href="https://github.com/fzaninotto/Faker/blob/master/readme.md" target="_blank"><?= __('Faker docs','fakingContent') ?></a></small></p>
-            </div>
-        </div>
-    </body>
-</html>
+echo $twig->render($template,$data);
