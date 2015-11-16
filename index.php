ae5f1b42da4d67a004479623eef3ca39cfbc1f1b
alanpilloud <alanp@rc2.ch>
2015-11-16 10:31:32 +0100

    Added thumbnail image management

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
+require __DIR__. '/../wp-load.php';
+$upload_dir = wp_upload_dir();
+define('UPLOAD_DIR', $upload_dir['path']);
+
 require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/config.php';
-require __DIR__. '/../wp-load.php';
 
 Twig_Autoloader::register();
 $loader = new Twig_Loader_Filesystem('templates');
@@ -10,6 +13,8 @@ $twig = new Twig_Environment($loader);
 $faking_content_url = basename($_SERVER['PHP_SELF']);
 
 if (filter_input(INPUT_GET, 'proceed') == 1) {
+    include(ABSPATH.'wp-admin/includes/image.php'); 
+    
     /**
      *  Generating content !
      */
@@ -26,12 +31,39 @@ if (filter_input(INPUT_GET, 'proceed') == 1) {
     	)
     );
     
+    /**
+     *  Generating post thumbnail !
+     */
+    if(POST_THUMBNAIL) {
+        $wp_filetype = wp_check_filetype(basename(POST_THUMBNAIL), null );
+        $attachment = array(
+            'post_mime_type' => $wp_filetype['type'],
+            'post_title' => POST_THUMBNAIL,
+            'post_content' => '',
+            'post_status' => 'inherit'
+        );
+        
+        $attach_id = wp_insert_attachment( $attachment, UPLOAD_DIR.'/'.POST_THUMBNAIL );
+        
+        $imagenew = get_post( $attach_id );
+        $fullsizepath = get_attached_file( $imagenew->ID );
+        $attach_data = wp_generate_attachment_metadata( $attach_id, $fullsizepath );
+        wp_update_attachment_metadata( $attach_id, $attach_data );
+        update_post_meta( $post_id, '_thumbnail_id', $attach_id ); // Associating attachement to the post
+    }
+    
+    /**
+     *  Generating Acf values !
+     */
     if(isset($acf_values)) {
         foreach($acf_values as $k => $v) {
             update_field($k, $v, $post_id);
         }
     }
     
+    /**
+     *  Prepare values for the template
+     */
     $data['post'] = [
         'id' => $post_id,
         'url' => get_permalink($post_id),
